56dc576159e5c7d565df975e4783d46c9c74ec31
Kok Sing Khong <khongks@gmail.com>
2021-11-19 18:10:38 +1100

    Add ibm-entitlement-key

---
+++ scripts/00-create-ibm-entitlement-key.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# https://myibm.ibm.com/products-services/containerlibrary
+
+entitlementKey=$1
+namespace=${2:-integration}
+
+# check for missing mandatory entitlement key
+if [ -z "$entitlementKey" ]
+then
+      echo "ERROR: missing ibm entitlement key argument, make sure to pass a key, ex: '-k mykey'"
+      echo "Get key from this https://myibm.ibm.com/products-services/containerlibrary"
+      exit 1;
+fi
+
+echo '----------------------------------------------------------------------'
+echo ' INFO: Create IBM Entitlement Key Secret'
+echo '----------------------------------------------------------------------'
+# Create IBM Entitlement Key Secret
+oc create secret docker-registry ibm-entitlement-key \
+    --docker-username=cp \
+    --docker-password=$entitlementKey \
+    --docker-server=cp.icr.io \
+    --namespace=$namespace
